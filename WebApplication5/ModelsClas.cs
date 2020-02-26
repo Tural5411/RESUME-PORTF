9c0ce8551aee87d12cf8d1c92425199b60215436
Tural Abdulxaliqov <Tural_5411@mail.ru>
2020-02-26 18:05:23 +0400

    MVC

---
+++ WebApplication5/ModelsClas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication5.Model;
+
+namespace WebApplication5
+{
+    public class ModelsClas
+    {
+        ResumeDB db = new ResumeDB();
+        public tbl_CV CV { get; set; }
+        public tbl_CVPhoto CVPHOTO  {get; set;}
+        public tbl_AboutMe AboutMe { get; set; }
+        public tbl_AboutMePhoto AboutMePhoto { get; set; }
+        public List<tbl_WhatIDo> WhatIDo { get; set; }
+        public List<tbl_SkillLeft> SkillLeft { get; set; }
+        public List<tbl_SkillRight> SkillRight { get; set; }
+        public List<tbl_Education> Educations { get; set; }
+        public List<tbl_Work> Works { get; set; }
+        public List<tbl_Pricing> Pricings { get; set; }
+        public List<tbl_Post> Posts { get; set; }
+        public List<tbl_Client> Clients { get; set; }
+        public tbl_Contact Contact { get; set; } 
+            
+    }
+}
